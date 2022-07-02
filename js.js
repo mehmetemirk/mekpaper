91a94e7c58be35a98c045180741b44fe6456cd27
BLACKWOLF280 <107998676+BLACKWOLF280@users.noreply.github.com>
2022-07-02 21:20:10 +0300

    V1

---
+++ js.js
@@ -0,0 +1,62 @@
+var img = document.getElementById("foto").offsetHeight
+
+
+
+var hesap = document.getElementById("hesap").offsetHeight
+
+
+
+
+console.log(img)
+console.log(hesap)
+console.log(( a = img - hesap) / 2 + "px")
+
+
+
+var hesapdiv = document.getElementById("hesap")
+hesapdiv.style.marginTop = ( a = img - hesap) / 2 + "px"
+hesapdiv.style.marginBottom = ( a = img - hesap) / 2 + "px"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
